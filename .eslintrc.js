dc2ca1b161823f2e29bf897ecaba94a6ef43eef6
LMansoldo <lucasmansoldo0@gmail.com>
2021-09-05 21:40:12 -0300

    build: 1st commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+	},
+	extends: ['airbnb', 'prettier', 'plugin:react/recommended'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{ singleQuote: true, parse: 'flow' },
+			{ jsxSingleQuote: true },
+		],
+		'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+		'react/react-in-jsx-scope': 'off',
+		'import/prefer-default-export': 'off',
+		'consistent-return': 'off',
+		'no-use-before-define': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'global-require': 'off',
+		'import/no-dynamic-require': 'off',
+		'react/no-array-index-key': 'off',
+		'import/no-unresolved': 'off',
+		'import/extensions': 'off',
+		'no-plusplus': 'off',
+		'no-restricted-syntax': 'off',
+		'guard-for-in': 'off',
+		'react/no-unescaped-entities': 'off',
+		'jsx-a11y/click-events-have-key-events': 'off',
+		'jsx-a11y/no-static-element-interactions': 'off',
+	},
+};
